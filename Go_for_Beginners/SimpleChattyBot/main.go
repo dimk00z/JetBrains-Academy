52df900abe7113de8263ac3d272b16032e361a55
dimk00z <dimk00z@gmail.com>
2021-12-29 11:03:15 +0300

    Add task 2

--- Go_for_Beginners/SimpleChattyBot/main.go
+++ Go_for_Beginners/SimpleChattyBot/main.go
@@ -6,5 +6,9 @@ func main() {
 	var bot_name string = "Aid"
 	var birth_year int = 2021
 	fmt.Printf("Hello! My name is %s\n", bot_name)
-	fmt.Printf("I was created in %d.", birth_year)
+	fmt.Printf("I was created in %d.\n", birth_year)
+	fmt.Println("Please, remind me your name.")
+	var user_name string
+	fmt.Scan(&user_name)
+	fmt.Printf("What a great name you have, %s!", user_name)
 }
