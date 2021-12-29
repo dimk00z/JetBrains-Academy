19daf5abfaeb1dd7ef42d5aa5e4eaf18309fbad9
dimk00z <dimk00z@gmail.com>
2021-12-29 09:58:39 +0300

    Add first task

---
+++ Go_for_Beginners/SimpleChattyBot/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bot_name string = "Aid"
+	var birth_year int = 2021
+	fmt.Printf("Hello! My name is %s\n", bot_name)
+	fmt.Printf("I was created in %d.", birth_year)
+}
