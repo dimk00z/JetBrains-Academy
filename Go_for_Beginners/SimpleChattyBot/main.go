a503925ecfe7f52eed4fed119b09e1c656aeeaa4
dimk00z <dimk00z@gmail.com>
2021-12-29 16:06:24 +0300

    Add task 5

--- Go_for_Beginners/SimpleChattyBot/main.go
+++ Go_for_Beginners/SimpleChattyBot/main.go
@@ -5,33 +5,73 @@ import (
 	"strconv"
 )
 
-func main() {
-	var bot_name string = "Aid"
-	var birth_year int = 2021
-	fmt.Printf("Hello! My name is %s\n", bot_name)
-	fmt.Printf("I was created in %d.\n", birth_year)
-	fmt.Println("Please, remind me your name.")
-	var name string
-	fmt.Scan(&name)
+func greet(name, year string) {
+	fmt.Println("Hello! My name is " + name + ".")
+	fmt.Println("I was created in " + year + ".")
+}
 
+func showName() {
+	var name string
+	fmt.Println("Please, remind me your name.")
+	fmt.Scan(&name)
 	fmt.Println("What a great name you have, " + name + "!")
+}
+
+func guessAge() {
+	var rem3, rem5, rem7, age int
+
 	fmt.Println("Let me guess your age.")
 	fmt.Println("Enter remainders of dividing your age by 3, 5 and 7.")
-
-	var rem3, rem5, rem7, age int
 	fmt.Scan(&rem3, &rem5, &rem7)
 
 	age = (rem3*70 + rem5*21 + rem7*15) % 105
-
 	fmt.Println("Your age is " + strconv.Itoa(age) + "; that's a good time to start programming!")
+}
+
+func count() {
+	var n int
+
 	fmt.Println("Now I will prove to you that I can count to any number you want.")
-
-	// read a number and count to it here
-	var number int
-	fmt.Scan(&number)
-	for current_number := 0; current_number <= number; current_number++ {
-		fmt.Printf("%d !\n", current_number)
+	fmt.Scan(&n)
+	for i := 0; i <= n; i++ {
+		fmt.Printf("%d!\n", i)
 	}
+}
 
+func checkAnswer(correct_answer int) {
+	var answer int
+	for true {
+		fmt.Scanf("%d", &answer)
+		if answer == correct_answer {
+			break
+		}
+		fmt.Println("Please, try again.")
+	}
+}
+func startQuiz() {
+	fmt.Println("Let's test your programming knowledge.")
+	fmt.Println("Why do we use methods?")
+	questions := []string{
+		"1. To repeat a statement multiple times.",
+		"2. To decompose a program into several small subroutines.",
+		"3. To determine the execution time of a program.",
+		"4. To interrupt the execution of a program."}
+	for _, question := range questions {
+		fmt.Println(question)
+	}
+	checkAnswer(2)
 	fmt.Println("Completed, have a nice day!")
+}
+
+func sayGoodbye() {
+	fmt.Println("Congratulations, have a nice day!")
+}
+
+func main() {
+	greet("Aid", "2020") // change it as you need
+	showName()
+	guessAge()
+	count()
+	startQuiz()
+	sayGoodbye()
 }
