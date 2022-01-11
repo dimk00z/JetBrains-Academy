d2f1b972ed1266af0a22a499a5558c1f75c2f228
dimk00z <dimk00z@gmail.com>
2022-01-11 10:07:08 +0300

    Add Stage 2/6

--- Go_for_Beginners/CoffeeMachine/main.go
+++ Go_for_Beginners/CoffeeMachine/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func main() {
+func printCoffeeAlgoritm() {
 	coffee_algoritm := []string{
 		"Starting to make a coffee",
 		"Grinding coffee beans",
@@ -15,4 +18,20 @@ func main() {
 	for _, line := range coffee_algoritm {
 		fmt.Println(line)
 	}
+}
+func calculateIngredients() {
+	var cups int
+	fmt.Println("Write how many cups of coffee you will need:")
+	_, err := fmt.Scanf("%d", &cups)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("For %d cups of coffee you will need:\n", cups)
+	fmt.Printf("%d ml of water\n", cups*200)
+	fmt.Printf("%d ml of milk\n", cups*50)
+	fmt.Printf("%d g of coffee beans\n", cups*15)
+}
+func main() {
+	printCoffeeAlgoritm()
+	calculateIngredients()
 }
